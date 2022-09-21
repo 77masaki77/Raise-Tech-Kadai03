f3da77b5ba5e25676491fb79dd99bdadd57dc327
masaki-hayashi <masaki77_7@yahoo.co.jp>
2022-09-21 22:49:23 +0900

    課題3 Mapを作成、表示、および例外処理の実装

--- src/kadai03/Main.java
+++ src/kadai03/Main.java
@@ -1,7 +1,45 @@
 package kadai03;
 
-public class Main {
-    public static void main(String[] args){
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> beerMap = new HashMap<>();
+        beerMap.put("キリン", null);
+        beerMap.put("アサヒ", null);
+        beerMap.put("サッポロ", null);
+
+        System.out.println("＜ビールメーカーと銘柄のリスト作成＞");
+        System.out.println("以下３社のお好きな銘柄を入力願います。");
+
+        for (String key : beerMap.keySet()) {
+            System.out.print(key + "といえば : ");
+            String commandLine = new Scanner(System.in).nextLine();
+            try {
+                checkBlankMakeError(commandLine);
+            } catch (Exception e) {
+                System.out.println(e);
+                commandLine = "未回答";
+            }
+            beerMap.replace(key, commandLine);
+        }
+
+        //Mapの要素を表示
+        System.out.printf("%n*ビールメーカー、銘柄リストの表示*%n");
+        System.out.println("-------------------------------");
+        System.out.println("key 　 : value");
+        System.out.println("-------------------------------");
+        for (String key : beerMap.keySet()) {
+            System.out.printf("%-4s : %s%n", key, beerMap.get(key));
+        }
+        System.out.println("-------------------------------");
+    }
+
+    public static void checkBlankMakeError(String commandLine) throws Exception {
+        if (commandLine.isBlank())  {
+            throw new Exception("スペースか、何も入力がありません。未回答とします。");
+        }
     }
 }
